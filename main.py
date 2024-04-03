94408cec15156963000197356cd592878e2e525d
shaan <sanu.bhattacharya@vacobinary.in>
2024-04-03 12:17:51 +0530

    Attribute Error occuring
    Signed-off-by: shaan <sanu.bhattacharya@vacobinary.in>

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 from bs4 import BeautifulSoup
-import urllib3
 import requests
+import csv
 
 def fetch_html_data():
     links = [
         "https://www.amazon.com/dp/0142501417?psc=1&th=1&vs=1",
         "https://www.amazon.com/dp/0316428388?psc=1&th=1&vs=1",
         "https://www.amazon.com/dp/0486256936?psc=1&th=1&vs=1",
-        "https://www.amazon.com/dp/0545685451?psc=1&th=1&vs=1",
-        "https://www.amazon.com/dp/0634021427?psc=1&th=1&vs=1",
-        "https://www.amazon.com/dp/0877793522?psc=1&th=1&vs=1",
-        "https://www.amazon.com/dp/1223062341?psc=1&th=1&vs=1",
-        "https://www.amazon.com/dp/1250201381?psc=1&th=1&vs=1",
-        "https://www.amazon.com/dp/1250829216?psc=1&th=1&vs=1",
-        "https://www.amazon.com/dp/1328910474?psc=1&th=1&vs=1",
-        "https://www.amazon.com/dp/1338106449?psc=1&th=1&vs=1",
     ]
 
     for urls in links:
         url = 'https://api.crawlbase.com/?token=lrnHZ2r3ZkaBWJcIA_W9oQ&url=' + urls
         responses = requests.get(str(url))
-        print(responses.text)
+        render_data(responses)
 
-def render_data():
-    with open('test2.html') as html_file:
-        soup = BeautifulSoup(html_file, 'html.parser')
-        book_info = {}
-        for item in soup.find('div', {'id': 'detailBullets_feature_div'}).find_all('li'):
-            try:
-                key = item.text.split(':')[0].strip()
-                value = item.text.split(':')[1].strip()
-                book_info[key] = value
-            except IndexError:
-                print(f"Error: {item.text} does not have a value.")
 
-        for keys, values in book_info.items():
-            print(keys, values)
+def render_data(values):
+    soup = BeautifulSoup(values.text, 'lxml')
+    book_info = {}
+    for item in soup.find('div', {'id': 'detailBullets_feature_div'}).find_all('li'):
+        try:
+            key = item.text.split(':')[0].strip()
+            value = item.text.split(':')[1].strip()
+            book_info[key] = value
+        except IndexError:
+            print(f"Error: {item.text} does not have a value.")
+
+    for keys, values in book_info.items():
+        print(keys, values)
+
 
 if __name__ == '__main__':
-    render_data()
     fetch_html_data()
-
