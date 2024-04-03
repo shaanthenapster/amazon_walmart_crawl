627eb418d2db644ed2b1e3f270bda91ce42e172d
shaan <sanu.bhattacharya@vacobinary.in>
2024-04-03 10:55:06 +0530

    [core: crawlbase api added , urls are fetching responses]
    ;
    
    Signed-off-by: shaan <sanu.bhattacharya@vacobinary.in>

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 from bs4 import BeautifulSoup
+import urllib3
+import requests
+
+def fetch_html_data():
+    links = [
+        "https://www.amazon.com/dp/0142501417?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/0316428388?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/0486256936?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/0545685451?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/0634021427?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/0877793522?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/1223062341?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/1250201381?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/1250829216?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/1328910474?psc=1&th=1&vs=1",
+        "https://www.amazon.com/dp/1338106449?psc=1&th=1&vs=1",
+    ]
+
+    for urls in links:
+        url = 'https://api.crawlbase.com/?token=lrnHZ2r3ZkaBWJcIA_W9oQ&url=' + urls
+        responses = requests.get(str(url))
+        print(responses.text)
 
 def render_data():
     with open('test2.html') as html_file:
@@ -17,4 +39,5 @@ def render_data():
 
 if __name__ == '__main__':
     render_data()
+    fetch_html_data()
 
